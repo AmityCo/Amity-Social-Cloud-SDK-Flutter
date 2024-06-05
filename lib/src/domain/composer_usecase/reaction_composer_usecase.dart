d30ffb7bf0f761378e33909a46570a433fdd5d69
Zryte <filmmanuchet@gmail.com>
2024-06-05 15:08:09 +0700

    Add reaction creator as linked object (#329)
    Co-authored-by: Zryte <Zryte@gmail.com>

--- lib/src/domain/composer_usecase/reaction_composer_usecase.dart
+++ lib/src/domain/composer_usecase/reaction_composer_usecase.dart
@@ -4,9 +4,11 @@ import 'package:amity_sdk/src/domain/domain.dart';
 class ReactionComposerUsecase extends UseCase<AmityReaction, AmityReaction> {
   final ReactionRepo reactionRepo;
   final UserRepo userRepo;
+  final UserComposerUsecase userComposerUsecase;
 
   ReactionComposerUsecase({
     required this.reactionRepo,
+    required this.userComposerUsecase,
     required this.userRepo,
   });
 
@@ -15,6 +17,10 @@ class ReactionComposerUsecase extends UseCase<AmityReaction, AmityReaction> {
 
     //Compose the user info
     final user = await userRepo.getUserByIdFromDb(params.userId!);
+
+    final composedUser = await userComposerUsecase.get(user);
+    params.creator = composedUser;
+
     params.userDisplayName = user.displayName ?? "";
 
     return params;
